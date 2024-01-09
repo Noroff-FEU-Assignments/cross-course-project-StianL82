3b97290cf5e9fda14adbf47fef8028592de404dd
StianL82 <stian.lilleng@gmail.com>
2024-01-09 18:38:45 +0100

    Update render_html.js

--- js/data/render_html.js
+++ js/data/render_html.js
@@ -1,27 +1,27 @@
+
 export const movieHTML = (movie) => {
   let price;
-  if (movie.onSale) {
-    price = movie.discountedPrice;
+  if (movie.on_sale) {
+    price = movie.prices.sale_price;
   } else {
-    price = movie.price;
+    price = movie.prices.price;
   }
 
   return `
     <div class="movie">
       <div class="moviesImage">
-        <a href="/products/movie.html?id=${movie.id}">
+        <a href="/products/movie.html?id=${movie.id.toString()}">
           <img
-            src="${movie.image}"
-            alt="${movie.title}"
+            src="${movie.images[0].src}"
+            alt="${movie.name}"
           />
         </a>
       </div>  
-      <p>${movie.title}</p>
-      <h3>$${price}</h3>
+      <p>${movie.name}</p>
+      <h3>kr ${ (price / 100).toFixed(2) }</h3>
     </div>
   `;
 };
-
 
 export function renderMovie(container, movie) {
   container.innerHTML += movieHTML(movie);
